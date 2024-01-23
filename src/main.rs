204197c69b255a19254e8d6767d3d5f4e2d0efc7
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-01-23 01:14:44 +0100

    implement eval; bugfixes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#![feature(exclusive_range_pattern)]
-use std::io::{stdout, Write};
+#![feature(exclusive_range_pattern, type_alias_impl_trait)]
+use std::cmp::Reverse;
 
 use clap::Parser;
-use colored::Colorize;
 use itertools::Itertools;
+use serde_derive::Serialize;
 
 /// Generate a random string.
 fn generate_random_string(n: usize, sigma: usize) -> Vec<u8> {
@@ -12,9 +12,41 @@ fn generate_random_string(n: usize, sigma: usize) -> Vec<u8> {
         .collect()
 }
 
-fn minimal_rotation_pos(s: &[u8], r: usize) -> usize {
+fn h(kmer: &[u8]) -> u64 {
+    fxhash::hash64(kmer)
+}
+
+/// Rightmost element with minimal hash.
+fn minimizer(s: &[u8], k: usize) -> usize {
+    assert!(k > 0);
+    assert!(k <= s.len());
+    s.windows(k)
+        .enumerate()
+        .min_by_key(|&(i, kmer)| (h(kmer), Reverse(i)))
+        .unwrap()
+        .0
+}
+
+/// Rightmost element with minimal hash, or last if possible.
+fn robust_minimizer(s: &[u8], k: usize, last: &mut usize) -> usize {
+    assert!(k <= s.len());
+    let (i, kmer) = s
+        .windows(k)
+        .enumerate()
+        .min_by_key(|&(i, kmer)| (h(kmer), Reverse(i)))
+        .unwrap();
+    if *last > 0 && h(&s[*last - 1..*last - 1 + k]) == h(kmer) {
+        *last -= 1;
+    } else {
+        *last = i;
+    }
+    *last
+}
+
+fn bd_anchor(s: &[u8], r: usize) -> usize {
+    assert!(r <= s.len());
     let mut best = 0;
-    for i in 1..s.len() - r {
+    for i in 1..s.len().saturating_sub(r) {
         for j in 0..s.len() {
             if s[(i + j) % s.len()] != s[(best + j) % s.len()] {
                 if s[(i + j) % s.len()] < s[(best + j) % s.len()] {
@@ -27,27 +59,45 @@ fn minimal_rotation_pos(s: &[u8], r: usize) -> usize {
     best
 }
 
-fn minimizer_pos(s: &[u8], k: usize) -> usize {
+/// 1. Select kmers whose k0-minimizer is at their start or end.
+/// 2. From these, select a minimal one.
+// TODO: Same hash or distinct hashes?
+fn miniception(s: &[u8], k: usize, k0: usize) -> usize {
+    let w = s.len() - k + 1;
+    assert!(k0 >= k.saturating_sub(w));
+    assert!(k0 <= k);
+
+    let w0 = k - k0;
+    let _l0 = w0 + k0 - 1;
+
+    // Alternate hash function.
+    let h1 = |x: &[u8]| fxhash::hash64(&fxhash::hash64(x));
+
     s.windows(k)
         .enumerate()
-        .min_by_key(|(_, w)| fxhash::hash64(w))
+        .filter(|(_, kmer)| {
+            let i = minimizer(kmer, k0);
+            assert!(i <= k - k0);
+            i == 0 || i == k - k0
+        })
+        .min_by_key(|&(i, w)| (h1(w), Reverse(i)))
         .unwrap()
         .0
 }
 
-fn biminimizer_pos(s: &[u8], k: usize, last: &mut usize) -> usize {
+fn robust_biminimizer_pos(s: &[u8], k: usize, last: &mut usize) -> usize {
     let h1 = |x: &[u8]| fxhash::hash64(x);
     let h2 = |x: &[u8]| fxhash::hash64(&fxhash::hash64(x));
     let i1 = s
         .windows(k)
         .enumerate()
-        .min_by_key(|(_, w)| h1(w))
+        .min_by_key(|&(i, w)| (h1(w), Reverse(i)))
         .unwrap()
         .0;
     let i2 = s
         .windows(k)
         .enumerate()
-        .min_by_key(|(_, w)| h2(w))
+        .min_by_key(|&(i, w)| (h2(w), Reverse(i)))
         .unwrap()
         .0;
     if *last == i1 + 1 || *last == i2 + 1 {
@@ -58,202 +108,205 @@ fn biminimizer_pos(s: &[u8], k: usize, last: &mut usize) -> usize {
     *last
 }
 
-fn reduced_minimizer_pos(s: &[u8], k: usize, r: usize) -> usize {
+fn reduced_minimizer(s: &[u8], k: usize, k0: usize) -> usize {
+    let l = s.len();
+    let _w = l - k + 1;
+    assert!(k0 <= k);
+    assert!(k <= (l + k0 + 1) / 2);
     let idx = s
-        .windows(r)
+        .windows(k0)
         .enumerate()
-        .min_by_key(|(_, w)| fxhash::hash64(w))
+        .min_by_key(|&(i, w)| (fxhash::hash64(w), Reverse(i)))
         .unwrap()
         .0;
-    let a = (2 * s.len() + r) * 1 / 3;
-    let b = (2 * s.len() + r) * 2 / 3;
-    let pos = 2 * idx + r;
-    let i = if pos < a {
-        idx
-    } else if pos < b {
-        idx - (k - r) / 2
-    } else {
-        idx - (k - r)
-    };
-    // let i = if idx > s.len() - k {
-
-    //     // if idx >= k - r {
-    //     //     idx - k + r
-    //     // } else {
-    //     //     idx - k / 2 + r
-    //     // }
-    // } else {
-    //     idx
-    // };
-    assert!(i <= s.len());
-    assert!(
-        i + k <= s.len(),
-        "len={} idx={idx} k={k} r={r} pos={pos} i={i}",
-        s.len()
-    );
+    let i = if idx + k <= l { idx } else { idx - (k - k0) };
+    assert!(i <= l, "l={l} idx={idx} k={k} k0={k0} i={i}");
+    assert!(i + k <= l, "l={l} idx={idx} k={k} k0={k0} i={i}");
     i
 }
 
-#[derive(clap::ValueEnum, Clone, Copy, Default)]
-enum Type {
+/// Compute the density of the sampling scheme.
+/// A function to select the minimal kmer from an lmer.
+/// The length of the slice is l.
+/// k must be bound by the function.
+///
+/// Must return a value in [0, l - k].
+fn density(text: &[u8], l: usize, scheme: impl Fn(&[u8]) -> usize) -> f64 {
+    // Collect positions.
+    let mut anchors = text
+        .windows(l)
+        .enumerate()
+        .map(|(i, w)| i + scheme(w))
+        .dedup()
+        .collect_vec();
+
+    // Dedup anchors to ensure valid results for non-forward schemes (bd-anchors).
+    anchors.sort();
+    anchors.dedup();
+
+    anchors.len() as f64 / text.windows(l).len() as f64
+}
+
+#[derive(Clone, Copy, clap::Subcommand, Debug, Serialize)]
+#[serde(tag = "minimizer_type")]
+enum MinimizerType {
     Minimizer,
-    ReducedMinimizer,
+    RobustMinimizer,
+    BdAnchor { r: usize },
+    Miniception { k0: usize },
     BiMinimizer,
-    #[default]
-    BdAnchor,
+    ReducedMinimizer { k0: usize },
+}
+
+#[derive(Clone, Copy, Debug, Serialize)]
+struct Result {
+    k: usize,
+    w: usize,
+    l: usize,
+    tp: MinimizerType,
+    density: f64,
+}
+
+impl MinimizerType {
+    fn density(&self, text: &[u8], l: usize, k: usize) -> f64 {
+        match self {
+            MinimizerType::Minimizer => density(text, l, |lmer| minimizer(lmer, k)),
+            MinimizerType::RobustMinimizer => density(text, l, |lmer| {
+                let last = &mut 0;
+                robust_minimizer(lmer, k, last)
+            }),
+            MinimizerType::BdAnchor { r } => density(text, l, |lmer| bd_anchor(lmer, *r)),
+            MinimizerType::Miniception { k0 } => density(text, l, |lmer| miniception(lmer, k, *k0)),
+            MinimizerType::BiMinimizer => density(text, l, |lmer| {
+                let last = &mut 0;
+                robust_biminimizer_pos(lmer, k, last)
+            }),
+            MinimizerType::ReducedMinimizer { k0 } => {
+                density(text, l, |lmer| reduced_minimizer(lmer, k, *k0))
+            }
+        }
+    }
+
+    /// For BdAnchor and ReducedMinimizer, try r/k0 from 3 to 7.
+    /// For Miniception, try k0 from 3 to 7, and k0 = k-w = k-(l-k+1) = 2k-l-1.
+    fn try_params(&self, l: usize, k: usize) -> Vec<Self> {
+        match self {
+            MinimizerType::Minimizer
+            | MinimizerType::RobustMinimizer
+            | MinimizerType::BiMinimizer => vec![*self],
+            MinimizerType::BdAnchor { .. } => {
+                let r_max = k;
+                (0.min(r_max)..=10.min(r_max))
+                    .map(|r| MinimizerType::BdAnchor { r })
+                    .collect()
+            }
+            MinimizerType::Miniception { .. } => {
+                let k0_min = (2 * k).saturating_sub(l + 1);
+                let k0_max = k;
+                if k0_min > k0_max {
+                    return vec![];
+                }
+
+                let start = 1.max(k0_min);
+                let end = 10.max(k0_min + 2).min(k0_max);
+                (start..=end)
+                    .map(|k0| MinimizerType::Miniception { k0 })
+                    .collect()
+            }
+            MinimizerType::ReducedMinimizer { .. } => {
+                // k <= (l+k0+1)/2
+                // 2k <= l + k0 + 1
+                // 2k - l - 1 <= k0
+                let k0_min = 1.max((2 * k - 1).saturating_sub(l));
+                let k0_max = 10.max(k0_min + 2).min(k);
+                (k0_min..=k0_max)
+                    .map(|k0| MinimizerType::ReducedMinimizer { k0 })
+                    .collect()
+            }
+        }
+    }
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    Run {
+        /// Test kmers up to l.
+        #[clap(short, default_value_t = 40)]
+        l: usize,
+        /// Minimizer length.
+        #[arg(short, default_value_t = 10)]
+        k: usize,
+        #[clap(subcommand)]
+        tp: MinimizerType,
+    },
+    Eval,
 }
 
 /// Print expected and measured densities for different l and r.
 #[derive(clap::Parser)]
 struct Args {
     /// Length of the generated random string.
-    #[arg(short, default_value_t = 100000)]
+    #[clap(short, default_value_t = 100000)]
     n: usize,
     /// Alphabet size.
-    #[arg(short, long, default_value_t = 4)]
+    #[clap(short, long, default_value_t = 4)]
     sigma: usize,
-    /// Test kmers up to l.
-    #[arg(short, default_value_t = 40)]
-    l: usize,
-    /// Test r (reduction constant) up to this.
-    #[arg(short, default_value_t = 10)]
-    r: usize,
-    /// Minimizer length.
-    #[arg(short, default_value_t = 10)]
-    k: usize,
-    /// The type of anchor to use.
-    #[arg(short, value_enum, default_value_t = Type::default())]
-    t: Type,
+    #[clap(subcommand)]
+    command: Command,
 }
 
 fn main() {
     let args = Args::parse();
-    let t = generate_random_string(args.n, args.sigma);
+    let text = &generate_random_string(args.n, args.sigma);
 
-    match args.t {
-        Type::BdAnchor => {
-            print!(" l ");
-            for r in args.r..=args.r {
-                print!("        r={r:>2}");
-            }
-            println!();
-            for l in args.l..=args.l {
-                print!("{l:>2}:");
-                let mut last_density = 2.;
-                for r in args.r..=args.r.min(l - 1) {
-                    let anchors: Vec<_> = t
-                        .windows(l)
-                        .enumerate()
-                        .map(|(i, w)| i + minimal_rotation_pos(w, r))
-                        .dedup()
-                        .collect();
-                    let exp = 2. / (l + 1 - r) as f64;
-                    let density = anchors.len() as f64 / (t.len() - l + 1) as f64;
-                    let density_str = if density < last_density {
-                        format!("{density:>.3}").green()
-                    } else {
-                        format!("{density:>.3}").red()
-                    };
-                    print!(" {}/{density_str}", format!("{exp:>.3}").dimmed(),);
-                    last_density = density;
-                    stdout().flush().unwrap();
-                }
-                println!();
-            }
+    match args.command {
+        Command::Run { tp, l, k } => {
+            eprintln!("Running {tp:?}:");
+            let d = tp.density(text, l, k);
+            eprintln!("  Density: {:.3}", d);
         }
+        Command::Eval => {
+            let base_types = [
+                MinimizerType::Minimizer,
+                MinimizerType::RobustMinimizer,
+                MinimizerType::BdAnchor { r: 0 },
+                MinimizerType::Miniception { k0: 0 },
+                MinimizerType::BiMinimizer,
+                MinimizerType::ReducedMinimizer { k0: 0 },
+            ];
 
-        Type::Minimizer => {
-            print!(" l ");
-            for k in args.k..=args.k {
-                print!("        k={k:>2}");
-            }
-            println!();
-            for l in args.l..=args.l {
-                print!("{l:>2}:");
-                let mut last_density = 2.;
-                for k in args.k..=args.k {
-                    let anchors: Vec<_> = t
-                        .windows(l)
-                        .enumerate()
-                        .map(|(i, w)| i + minimizer_pos(w, k))
-                        .dedup()
-                        .collect();
-                    let exp = 2. / (l + 2 - k) as f64;
-                    let density = anchors.len() as f64 / (t.len() - l + 1) as f64;
-                    let density_str = if density < last_density {
-                        format!("{density:>.3}").green()
-                    } else {
-                        format!("{density:>.3}").red()
-                    };
-                    print!(" {}/{density_str}", format!("{exp:>.3}").dimmed(),);
-                    last_density = density;
-                    stdout().flush().unwrap();
-                }
-                println!();
-            }
-        }
+            let mut results = vec![];
 
-        Type::BiMinimizer => {
-            print!(" l ");
-            for k in args.k..=args.k {
-                print!("        k={k:>2}");
-            }
-            println!();
-            for l in args.l..=args.l {
-                print!("{l:>2}:");
-                let mut last_density = 2.;
-                for k in args.k..=args.k {
-                    let last = &mut 0;
-                    let anchors: Vec<_> = t
-                        .windows(l)
-                        .enumerate()
-                        .map(|(i, w)| i + biminimizer_pos(w, k, last))
-                        .dedup()
-                        .collect();
-                    let exp = 2. / (l + 2 - k) as f64;
-                    let density = anchors.len() as f64 / (t.len() - l + 1) as f64;
-                    let density_str = if density < last_density {
-                        format!("{density:>.3}").green()
-                    } else {
-                        format!("{density:>.3}").red()
-                    };
-                    print!(" {}/{density_str}", format!("{exp:>.3}").dimmed(),);
-                    last_density = density;
-                    stdout().flush().unwrap();
+            for k in [1, 2, 4, 8, 16, 32, 64, 128] {
+                for w in [1, 2, 4, 8, 16, 32, 64, 128] {
+                    let l = k + w - 1;
+                    for tp in base_types.iter() {
+                        let tps = &tp.try_params(l, k);
+                        let (d, tp) = tps
+                            .iter()
+                            .map(|tp| (tp.density(text, l, k), tp))
+                            .min_by(|&(ld, _), &(rd, _)| {
+                                if ld < rd {
+                                    std::cmp::Ordering::Less
+                                } else {
+                                    std::cmp::Ordering::Greater
+                                }
+                            })
+                            .unwrap();
+                        results.push(Result {
+                            k,
+                            w,
+                            l,
+                            tp: *tp,
+                            density: d,
+                        });
+                        eprintln!("k={k} w={w} l={l} tp={tp:?} d={d:.3}",);
+                    }
                 }
-                println!();
             }
-        }
 
-        Type::ReducedMinimizer => {
-            print!(" l ");
-            for k in args.k..=args.k {
-                print!("        k={k:>2}");
-            }
-            println!();
-            for l in args.l..=args.l {
-                print!("{l:>2}:");
-                let mut last_density = 2.;
-                for k in args.k..=args.k {
-                    let anchors: Vec<_> = t
-                        .windows(l)
-                        .enumerate()
-                        .map(|(i, w)| i + reduced_minimizer_pos(w, k, args.r))
-                        .dedup()
-                        .collect();
-                    let exp = 2. / (l + 2 - k) as f64;
-                    let density = anchors.len() as f64 / (t.len() - l + 1) as f64;
-                    let density_str = if density < last_density {
-                        format!("{density:>.3}").green()
-                    } else {
-                        format!("{density:>.3}").red()
-                    };
-                    print!(" {}/{density_str}", format!("{exp:>.3}").dimmed(),);
-                    last_density = density;
-                    stdout().flush().unwrap();
-                }
-                println!();
-            }
+            let result_json = serde_json::to_string(&results).unwrap();
+            println!("{}", result_json);
         }
     }
 }
