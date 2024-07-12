fb95c3444ba191ab400ed25a8843612dd822dc2d
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-07-12 23:51:43 +0200

    skip naive implementations

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,3 +1,4 @@
+#![allow(unused_imports)]
 mod randmini;
 
 use criterion::{criterion_group, criterion_main, Criterion};
@@ -19,18 +20,18 @@ fn bench(c: &mut Criterion) {
     let w = 11;
 
     let mut g = c.benchmark_group("g");
-    g.bench_function("0_naive_lex", |b| {
-        let m = V0NaiveLex { w, k };
-        b.iter(|| m.minimizers(text));
-    });
-    g.bench_function("1_naive_fx", |b| {
-        let m = V1NaiveFx { w, k };
-        b.iter(|| m.minimizers(text));
-    });
-    g.bench_function("2_naive_wy", |b| {
-        let m = V2NaiveWy { w, k };
-        b.iter(|| m.minimizers(text));
-    });
+    // g.bench_function("0_naive_lex", |b| {
+    //     let m = V0NaiveLex { w, k };
+    //     b.iter(|| m.minimizers(text));
+    // });
+    // g.bench_function("1_naive_fx", |b| {
+    //     let m = V1NaiveFx { w, k };
+    //     b.iter(|| m.minimizers(text));
+    // });
+    // g.bench_function("2_naive_wy", |b| {
+    //     let m = V2NaiveWy { w, k };
+    //     b.iter(|| m.minimizers(text));
+    // });
 
     g.bench_function("ext_minimizer_iter", |b| {
         b.iter(|| {
