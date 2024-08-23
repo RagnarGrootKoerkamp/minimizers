f2d6583ceef38c2ee32dc640d44d5f99c0f5e218
Bryce Lorenz Kille <brycekille@gmail.com>
2024-08-23 15:11:23 -0500

    Use fixed seed for random text generation

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
     fs,
 };
 
+use rand::{Rng, SeedableRng};
 use clap::Parser;
 use itertools::Itertools;
 use minimizers::{de_bruijn_seq::de_bruijn_sequence, *};
@@ -15,8 +16,9 @@ use serde_derive::Serialize;
 
 /// Generate a random string.
 fn generate_random_string(n: usize, sigma: usize) -> Vec<u8> {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(213456);
     (0..n)
-        .map(|_| (rand::random::<usize>() % sigma) as u8)
+        .map(|_| (rng.gen::<usize>() % sigma) as u8)
         .collect()
 }
 
