bd601376a6fa50540da725d632502ea45e5b89da
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-12-12 17:24:24 +0100

    Fix inlining issues

--- src/simd/minimizer.rs
+++ src/simd/minimizer.rs
@@ -8,6 +8,7 @@ use super::{
 use core::array::from_fn;
 use itertools::Itertools;
 use packed_seq::{Seq, S};
+use std::hint::assert_unchecked;
 
 /// A custom RingBuf implementation that has a fixed size `w` and wraps around.
 struct RingBuf<V> {
@@ -32,7 +33,7 @@ impl<V: Clone> RingBuf<V> {
 
     #[inline(always)]
     fn push(&mut self, v: V) {
-        self.data[self.idx] = v;
+        *unsafe { self.data.get_unchecked_mut(self.idx) } = v;
         self.idx += 1;
         if self.idx == self.w {
             self.idx = 0;
@@ -135,26 +136,24 @@ fn sliding_min_par_it(
         move |val| {
             // Make sure the position does not interfere with the hash value.
             if pos == max_pos {
-                let delta = S::splat((1 << 16) - 2 - w as u32);
-                pos -= delta;
-                prefix_min -= delta;
-                pos_offset += delta;
-                for x in &mut *ring_buf {
-                    *x -= delta;
-                }
+                // Slow case extracted to a function to have better inlining here.
+                reset_positions_offsets(
+                    w,
+                    &mut pos,
+                    &mut prefix_min,
+                    &mut pos_offset,
+                    &mut ring_buf,
+                );
             }
+            // slightly faster than assigning S::splat(u32::MAX)
             let elem = (val & val_mask) | pos;
             pos += S::splat(1);
             ring_buf.push(elem);
             prefix_min = prefix_min.min(elem);
             // After a chunk has been filled, compute suffix minima.
             if ring_buf.idx() == 0 {
-                let mut suffix_min = ring_buf[w - 1];
-                for i in (0..w - 1).rev() {
-                    suffix_min = suffix_min.min(ring_buf[i]);
-                    ring_buf[i] = suffix_min;
-                }
-                prefix_min = elem; // slightly faster than assigning S::splat(u32::MAX)
+                // Slow case extracted to a function to have better inlining here.
+                suffix_minima(&mut ring_buf, w, &mut prefix_min, elem);
             }
             let suffix_min = unsafe { *ring_buf.get_unchecked(ring_buf.idx()) };
             (prefix_min.min(suffix_min) & pos_mask) + pos_offset
@@ -163,6 +162,34 @@ fn sliding_min_par_it(
     // This optimizes better than it.skip(w-1).
     it.by_ref().take(w - 1).for_each(drop);
     it
+}
+
+fn suffix_minima(ring_buf: &mut RingBuf<S>, w: usize, prefix_min: &mut S, elem: S) {
+    // Avoid some bounds checks when this function is not inlined.
+    unsafe { assert_unchecked(ring_buf.len() == w) };
+    unsafe { assert_unchecked(w > 0) };
+    let mut suffix_min = ring_buf[w - 1];
+    for i in (0..w - 1).rev() {
+        suffix_min = suffix_min.min(ring_buf[i]);
+        ring_buf[i] = suffix_min;
+    }
+    *prefix_min = elem;
+}
+
+fn reset_positions_offsets(
+    w: usize,
+    pos: &mut S,
+    prefix_min: &mut S,
+    pos_offset: &mut S,
+    ring_buf: &mut RingBuf<S>,
+) {
+    let delta = S::splat((1 << 16) - 2 - w as u32);
+    *pos -= delta;
+    *prefix_min -= delta;
+    *pos_offset += delta;
+    for x in &mut **ring_buf {
+        *x -= delta;
+    }
 }
 
 /// Returns the minimizer of a window using a naive linear scan.
