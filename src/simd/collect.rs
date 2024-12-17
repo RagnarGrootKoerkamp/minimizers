30f0db824106f3e48c1cc887d52bb73596863f38
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-12-17 14:49:53 +0100

    collect: rename variables in SIMD matrix transpose

--- src/simd/collect.rs
+++ src/simd/collect.rs
@@ -169,31 +169,31 @@ fn transpose(m: [S; 8]) -> [S; 8] {
     unsafe {
         use std::arch::x86_64::*;
         let m: [__m256; 8] = transmute(m);
-        let t0 = _mm256_unpacklo_ps(m[0], m[1]);
-        let t1 = _mm256_unpackhi_ps(m[0], m[1]);
-        let t2 = _mm256_unpacklo_ps(m[2], m[3]);
-        let t3 = _mm256_unpackhi_ps(m[2], m[3]);
-        let t4 = _mm256_unpacklo_ps(m[4], m[5]);
-        let t5 = _mm256_unpackhi_ps(m[4], m[5]);
-        let t6 = _mm256_unpacklo_ps(m[6], m[7]);
-        let t7 = _mm256_unpackhi_ps(m[6], m[7]);
-        let tt0 = _mm256_shuffle_ps(t0, t2, _mm_shuffle(1, 0, 1, 0));
-        let tt1 = _mm256_shuffle_ps(t0, t2, _mm_shuffle(3, 2, 3, 2));
-        let tt2 = _mm256_shuffle_ps(t1, t3, _mm_shuffle(1, 0, 1, 0));
-        let tt3 = _mm256_shuffle_ps(t1, t3, _mm_shuffle(3, 2, 3, 2));
-        let tt4 = _mm256_shuffle_ps(t4, t6, _mm_shuffle(1, 0, 1, 0));
-        let tt5 = _mm256_shuffle_ps(t4, t6, _mm_shuffle(3, 2, 3, 2));
-        let tt6 = _mm256_shuffle_ps(t5, t7, _mm_shuffle(1, 0, 1, 0));
-        let tt7 = _mm256_shuffle_ps(t5, t7, _mm_shuffle(3, 2, 3, 2));
-        let mut m: [__m256; 8] = [transmute([0; 8]); 8];
-        m[0] = _mm256_permute2f128_ps(tt0, tt4, 0x20);
-        m[1] = _mm256_permute2f128_ps(tt1, tt5, 0x20);
-        m[2] = _mm256_permute2f128_ps(tt2, tt6, 0x20);
-        m[3] = _mm256_permute2f128_ps(tt3, tt7, 0x20);
-        m[4] = _mm256_permute2f128_ps(tt0, tt4, 0x31);
-        m[5] = _mm256_permute2f128_ps(tt1, tt5, 0x31);
-        m[6] = _mm256_permute2f128_ps(tt2, tt6, 0x31);
-        m[7] = _mm256_permute2f128_ps(tt3, tt7, 0x31);
-        transmute(m)
+        let x0 = _mm256_unpacklo_ps(m[0], m[1]);
+        let x1 = _mm256_unpackhi_ps(m[0], m[1]);
+        let x2 = _mm256_unpacklo_ps(m[2], m[3]);
+        let x3 = _mm256_unpackhi_ps(m[2], m[3]);
+        let x4 = _mm256_unpacklo_ps(m[4], m[5]);
+        let x5 = _mm256_unpackhi_ps(m[4], m[5]);
+        let x6 = _mm256_unpacklo_ps(m[6], m[7]);
+        let x7 = _mm256_unpackhi_ps(m[6], m[7]);
+        let y0 = _mm256_shuffle_ps(x0, x2, _mm_shuffle(1, 0, 1, 0));
+        let y1 = _mm256_shuffle_ps(x0, x2, _mm_shuffle(3, 2, 3, 2));
+        let y2 = _mm256_shuffle_ps(x1, x3, _mm_shuffle(1, 0, 1, 0));
+        let y3 = _mm256_shuffle_ps(x1, x3, _mm_shuffle(3, 2, 3, 2));
+        let y4 = _mm256_shuffle_ps(x4, x6, _mm_shuffle(1, 0, 1, 0));
+        let y5 = _mm256_shuffle_ps(x4, x6, _mm_shuffle(3, 2, 3, 2));
+        let y6 = _mm256_shuffle_ps(x5, x7, _mm_shuffle(1, 0, 1, 0));
+        let y7 = _mm256_shuffle_ps(x5, x7, _mm_shuffle(3, 2, 3, 2));
+        let mut t: [__m256; 8] = [transmute([0; 8]); 8];
+        t[0] = _mm256_permute2f128_ps(y0, y4, 0x20);
+        t[1] = _mm256_permute2f128_ps(y1, y5, 0x20);
+        t[2] = _mm256_permute2f128_ps(y2, y6, 0x20);
+        t[3] = _mm256_permute2f128_ps(y3, y7, 0x20);
+        t[4] = _mm256_permute2f128_ps(y0, y4, 0x31);
+        t[5] = _mm256_permute2f128_ps(y1, y5, 0x31);
+        t[6] = _mm256_permute2f128_ps(y2, y6, 0x31);
+        t[7] = _mm256_permute2f128_ps(y3, y7, 0x31);
+        transmute(t)
     }
 }
